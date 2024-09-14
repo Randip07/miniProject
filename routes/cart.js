896e2f72d27ccfc02a6bca84cb2087009289f02e
randipdas <ranadipdas07@gmail.com>
2024-09-14 14:50:51 +0530

    profile page

--- routes/cart.js
+++ routes/cart.js
@@ -15,8 +15,22 @@ router.get("", wrapAsync (async (req, res) => {
     name: data.name,
   };
   let cartItems = data.cart
-  
-  // res.render("cart.ejs", { cart: cartItems, cusData });
+  let calucalatedPrice = {
+    platformFee : 10,
+    itemTotal : 0,
+    afterDiscount : 0,
+    sgst : 0,
+    cgst : 0,
+    totalPay : 0
+  }
+  for(item of cartItems){
+    calucalatedPrice.itemTotal += item.quantity*item.itemId.price
+    calucalatedPrice.afterDiscount += Math.ceil(item.quantity*(item.itemId.price - (item.itemId.price*(item.itemId.discount/100))));
+  }
+  calucalatedPrice.sgst = Math.ceil(calucalatedPrice.afterDiscount*(2.5/100));
+  calucalatedPrice.cgst = Math.ceil(calucalatedPrice.afterDiscount*(2.5/100));
+  calucalatedPrice.totalPay = Math.ceil(calucalatedPrice.afterDiscount + calucalatedPrice.sgst + calucalatedPrice.cgst); 
+  res.render("cart.ejs", { cart: cartItems, cusData , calucalatedPrice});
 }));
 
 
