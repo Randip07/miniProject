07ae6eaf636593ca30452c586d86dee491f3f9ff
randipdas <ranadipdas07@gmail.com>
2024-11-03 21:23:15 +0530

    updated 2.0

--- routes/profile.js
+++ routes/profile.js
@@ -8,13 +8,12 @@ const Customer = require("../models/customers.js");
 
 
 // Rendering client side profile page
-router.get("", wrapAsync(async (req, res) => {
-  let data = await Customer.findById("6725ba4e2cb5b4d3bab11c63").populate("orders").populate({
+router.get("", isLoggedIn, wrapAsync(async (req, res) => {
+  let data = await Customer.findById(req.session.userId).populate("orders").populate({
     path: 'orders',          // Populates items within each order
     populate: { path: 'items.itemId'}
   })
-  console.log(data);
-  
+  data.orders.sort((a, b) => new Date(b.date) - new Date(a.date));
   res.render("profile.ejs", { cusData : data});
 }));
 
